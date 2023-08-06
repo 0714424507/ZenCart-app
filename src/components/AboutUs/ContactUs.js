6816910066642dba5c7458d3d75eb6654c043bb6
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-08-06 05:09:40 +0300

    Initial code for About us, Contact, Help and Terms

---
+++ src/components/AboutUs/ContactUs.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './AboutUs.css';
+
+const ContactUs = () => {
+  return (
+    <div className="contact-us">
+      <h1>Contact Us</h1>
+      <p>
+        If you have any questions, feedback, or inquiries, feel free to reach out to us. Our dedicated support team is here to assist you.
+      </p>
+      <div className="contact-details">
+        <div className="contact-info">
+          <h2>Email</h2>
+          <p>info@zencart.com</p>
+        </div>
+        <div className="contact-info">
+          <h2>Support Email</h2>
+          <p>support@zencart.com</p>
+        </div>
+        <div className="contact-info">
+          <h2>Phone</h2>
+          <p>+254 (0) 712 345 678</p>
+          <p>+254 (0) 723 456 789</p>
+          <p>+254 (0) 790 987 654</p>
+        </div>
+        <div className="contact-info">
+          <h2>Address</h2>
+          <p>
+            Moringa School,
+            Nairobi, Kenya
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
