f8671169eba0d9e2b170954d6ce7fb4bc1d1d097
Sheila Seruya <Sheilaseruya@gmail.com>
2023-08-06 23:06:08 +0300

    working on homepage

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,47 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Categories.css';
+
 
 const Categories = () => {
   return (
     <div className = 'sidebar'>
       <h2>Categories</h2>
       <ul>
-        <li>
-          <Link to="/categories/womens-fashion">
+          <Link className="sidebar-links" to="/categories/womens-fashion">
             <i className="fas fa-female"></i> Women's Fashion
           </Link>
-        </li>
-        <li>
-          <Link to="/categories/mens-fashion">
+        
+          <Link className="sidebar-links" to="/categories/mens-fashion">
             <i className="fas fa-male"></i> Men's Fashion
           </Link>
-        </li>
-        <li>
-          <Link to="/categories/kids-and-toys">
+        
+          <Link className="sidebar-links" to="/categories/kids-and-toys">
             <i className="fas fa-child"></i> Kids and Toys
           </Link>
-        </li>
-        <li>
-          <Link to="/categories/home-and-office">
+       
+          <Link className="sidebar-links" to="/categories/home-and-office">
             <i className="fas fa-home"></i> Home and Office
           </Link>
-        </li>
-        <li>
-          <Link to="/categories/pets-and-outdoor">
+       
+          <Link className="sidebar-links" to="/categories/pets-and-outdoor">
             <i className="fas fa-paw"></i> Pets and Outdoor
           </Link>
-        </li>
-        <li>
-          <Link to="/categories/beauty-health-and-hair">
+      
+          <Link className="sidebar-links" to="/categories/beauty-health-and-hair">
             <i className="fas fa-heartbeat"></i> Beauty, Health, and Hair
           </Link>
-        </li>
-        <li>
-          <Link to="/categories/electronics-and-accessories">
+      
+          <Link className="sidebar-links"  to="/categories/electronics-and-accessories">
             <i className="fas fa-mobile-alt"></i> Electronics and Accessories
           </Link>
-        </li>
       </ul>
     </div>
   );
