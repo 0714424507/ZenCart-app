33ff2659dbe9c6550134c6907e8d6a02f2e21ceb
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-08-07 20:53:20 +0300

    Slideshow worksgit add /Users/jonathannjine/Desktop/FinalProject/Zencart-app/src/components/Slideshow.js /Users/jonathannjine/Desktop/FinalProject/Zencart-app/src/components/Slideshow.css /Users/jonathannjine/Desktop/FinalProject/Zencart-app/src/components/Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,25 +1,12 @@
 import React from 'react';
-// import img1 from "./images/img1.png";
-import { Link } from "react-router-dom";
 import Categories from './Categories';
 import ProductGrid from './Product/ProductGrid';
 import TopLayout from './TopLayout';
-// import LoadMore from './Product/LoadMore';
-// import Pagination from './Product/Pagination';
 import Slideshow from './Slideshow';
 
 function Homepage() {
   return (
     <div>
-
-      {/* <div className='container1'>
-        <img src={img1} alt="Logo" />
-        <div className='cart'>
-          <Link className="navbar-link" to="/cart">
-            <i className="fas fa-shopping-cart fa-2x"></i>
-          </Link>
-        </div>
-      </div> */}
       <TopLayout/>
       <div className='container2'>
         <div className=''>
@@ -30,8 +17,6 @@ function Homepage() {
        </div>
       
       </div>
-      
-        {/* <Pagination /> */}
         <div className='homegrid'>
         <ProductGrid />
         </div>
