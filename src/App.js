32bede64144d12b6fbe05e684b1a90e2f0f92a66
Mwanzia24 <mwanzaitom19@gmail.com>
2023-08-07 14:36:20 -0400

    adding seller dashboard route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom'; // Uncomment this line to import BrowserRouter
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import SellerDashboard from './components/SellerDashboard/SellerDashboard';
 
 const App = () => {
   return (
     <Router> 
       <div>
         <Navbar />
-        {/* Your other components and content here */}
+        
+        {/* Use Routes instead of directly rendering Route */}
+        <Routes>
+          {/* Add routes for different pages here */}
+          <Route path="/seller-dashboard" element={<SellerDashboard />} />
+          {/* Add other routes as needed */}
+        </Routes>
       </div>
     </Router>
   );
