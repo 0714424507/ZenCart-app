39db5285b11bc8906608a4710914740f95ef978e
Faith Mwangi <faith.mwangi01@student.moringaschool.com>
2023-08-10 03:24:41 +0300

    Clean up About us/Help/Contacts/T&C

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -6,24 +6,29 @@ const AboutUs = () => {
   return (
     <div>
     <SideBar/>
+    <div className='aboutus-body'>
     <div className="aboutus">
-        <h1>About Us</h1>
         <br/>
         <h3 className='welcome'>Welcome to our e-commerce platform!</h3>
         <br/>
         <p className='who'>
+          <h1>Who are we?</h1>
         We are a dedicated team of innovators passionate about empowering businesses to thrive in the digital world.
         </p>
         <p className='mission'>
+        <h1>What are we about?</h1>
         Our mission is to overcome the challenges faced by businesses in setting up and operating e-commerce platforms, particularly related to payment gateways.
         </p>
-        <p>
+        <p className='problem-statement'>
+        <h1>Problem Statement</h1>
         In many regions, businesses encounter hurdles in establishing a robust online presence and securing seamless payment processing. We recognized this pressing problem, and that's why we embarked on a journey to create a cutting-edge e-commerce website that addresses these challenges head-on.
         </p>
-        <p>
-        Our platform leverages the power of Cellulant APIs, ensuring reliable and secure payment processing for businesses of all sizes. We firmly believe that providing seamless payment gateways is crucial for businesses to showcase and sell their products online efficiently. By harnessing the potential of technology and collaboration, we aim to uplift businesses and connect them with their customers on a whole new level.
+        <p className='payment-statement'>
+        <h1>Secure Payment</h1>
+        Our platform leverages the power of Jenga APIs, ensuring reliable and secure payment processing for businesses of all sizes. We firmly believe that providing seamless payment gateways is crucial for businesses to showcase and sell their products online efficiently. By harnessing the potential of technology and collaboration, we aim to uplift businesses and connect them with their customers on a whole new level.
         </p>
-      <p>
+      <p className='zencart-experience'>
+      <h1>✨ The zen<span>cart</span> Experience ✨</h1>
         At our core, we prioritize user experience, security, and reliability. We strive to create a user-friendly interface that simplifies the online shopping experience for both businesses and customers. Our commitment to data security ensures that every transaction is protected, building trust and confidence among our users.
 
         As we continue to grow and evolve, we envision a future where every business, regardless of size or location, can access the immense opportunities offered by the digital landscape. We are driven by the belief that technology can be a powerful catalyst for economic growth and empowerment.
@@ -32,12 +37,13 @@ const AboutUs = () => {
       Join us on this exciting journey as we revolutionize e-commerce and pave the way for businesses to thrive in a digital world. Together, let's unlock the potential of limitless possibilities for businesses and create a brighter, more inclusive future.
         
       </p>
-      <p>
+      <p className='end'>
       <br/>
         Thank you for being a part of our story.
         <br/>
-        The ZenCart Team
+        The zen<span>cart</span> Team
       </p>
+    </div>
     </div>
     </div>
   );
--- src/components/AboutUs/Help.js
+++ src/components/AboutUs/Help.js
@@ -6,6 +6,8 @@ const Help = () => {
   return (
     <div>
     <SideBar/>
+
+    <div className='help-body'>
     <div className="help">
       <div className="help-buyers">
         <h1 className="sellers">For Sellers</h1>
@@ -15,15 +17,15 @@ const Help = () => {
             <ul>
                 <li>
                 Seller Registration: To begin, you need to register as a seller on our platform. Fill out the necessary information, and our team will review your application to ensure the quality and legitimacy of the products you offer.
-                </li><li>
+                </li><br/><li>
                 Product Listings: Once approved, you can start creating product listings. Provide accurate and detailed information about your products, including high-quality images, descriptions, and pricing. Engaging and informative listings help attract potential buyers.
-                </li><li>
+                </li><br/><li>
                 Manage Inventory: Keep track of your inventory to avoid overselling. Update the availability status of each product promptly to maintain a positive shopping experience for customers.
-                </li><li>
+                </li><br/><li>
                 Order Fulfillment: When a customer places an order, ensure you fulfill it promptly and accurately. Package the products securely to prevent damage during transit.
-                </li><li>
+                </li><br/><li>
                 Customer Support: Promptly respond to customer inquiries and provide excellent customer support. Address any issues or concerns in a professional and friendly manner.
-                </li><li>
+                </li><br/><li>
                 Secure Payment Processing: Rest assured that our platform utilizes secure payment gateways, ensuring safe and smooth transactions for both you and your customers.
                 </li>
             </ul>
@@ -38,18 +40,19 @@ const Help = () => {
             <ul>
                 <li>
         Browse Products: Explore our wide range of products from various sellers. Utilize search filters and categories to find exactly what you're looking for.
-        </li><li>
+        </li><br/><li>
         Product Details: Before making a purchase, carefully read the product descriptions, specifications, and customer reviews. Understanding the product's features and quality helps you make informed decisions.
-        </li><li>
+        </li><br/><li>
         Secure Payments: Our platform ensures secure payment processing using reliable payment gateways. You can confidently make purchases knowing your sensitive information is protected.
-        </li><li>
+        </li><br/><li>
         Order Tracking: After placing an order, you can track its status through your account. We'll provide updates on the shipment's progress until it reaches your doorstep.
-        </li><li>
+        </li><br/><li>
         Leave Feedback: Your feedback is valuable to us and other buyers. After receiving your order, consider leaving a review to help other shoppers and provide valuable insights to sellers.
                 </li>
             </ul>
         </p>
       </div>
+    </div>
     </div>
     </div>
   );
