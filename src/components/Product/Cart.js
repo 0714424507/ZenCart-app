f9a58983d36837895c4a67a40b7e5edbadb81644
Humphrey <humphrey.muhuri@student.moringaschool.com>
2023-08-10 09:35:08 +0300

    Adding the cart component

---
+++ src/components/Product/Cart.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useCart } from './CartContext';
+
+const Cart = () => {
+  const { cartItems, removeFromCart, clearCart, getSubtotal, updateQuantity } = useCart();
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        <div>
+          {cartItems.map((product) => (
+            <div key={product.id} className="cart-item">
+              <div className="product-details">
+                <img src={product.image} alt={product.name} className="product-image" />
+                <div className="product-info">
+                  <p className="product-name">{product.name}</p>
+                  <p className="product-price">${product.price}</p>
+                  <div className="quantity-control">
+                    <button onClick={() => updateQuantity(product.id, -1)}>-</button>
+                    <p className="product-quantity">{product.quantity}</p>
+                    <button onClick={() => updateQuantity(product.id, 1)}>+</button>
+                  </div>
+                  <button onClick={() => removeFromCart(product.id)} className="remove-button">
+                    Remove
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+          <p>Total Items: {cartItems.length}</p>
+          <p>Subtotal: ${getSubtotal()}</p>
+          <button onClick={clearCart} className="clear-cart-button">
+            Clear Cart
+          </button>
+          <button className="checkout-button">Checkout</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
